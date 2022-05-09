c94a24a56c0859f94553dc872efd5ba57aaacce8
mark <markpackham1@gmail.com>
2022-05-09 15:56:10 +0100

    remove content from css and create element in js
    https://youtu.be/p_6IuhmBsfc?t=1832

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const startNumber = 9;
 const flipCard = document.querySelector(".flip-card");
 const topHalf = flipCard.querySelector(".top");
 const bottomHalf = flipCard.querySelector(".bottom");
+const topFlip = document.createElement("div");
+topFlip.classList.add("top-flip");
+const bottomFlip = document.createElement("div");
+bottomFlip.classList.add("bottom-flip");
 
 topHalf.textContent = startNumber;
 bottomHalf.textContent = startNumber;
