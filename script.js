ac89698df1c5a2a5ba4c4c5e9b10a89163c6cba7
mark <markpackham1@gmail.com>
2022-05-09 15:38:07 +0100

    add event listeners for transitions
    https://youtu.be/p_6IuhmBsfc?t=1458

--- script.js
+++ script.js
@@ -4,4 +4,8 @@ const flipCard = document.querySelector(".flip-card");
 [...flipCard.children].forEach((child) => {
   child.textContent = startNumber;
 });
+flipCard.dataset.currentNumber = startNumber;
+flipCard.dataset.nextNumber = startNumber - 1;
 flipCard.classList.add("flip");
+flipCard.addEventListener("transitionstart", (e) => {});
+flipCard.addEventListener("transitionend", (e) => {});
