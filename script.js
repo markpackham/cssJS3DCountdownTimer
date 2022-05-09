6f7d8c61c78e5f89cf0271abccfedb2d8cf0c100
mark <markpackham1@gmail.com>
2022-05-09 15:28:01 +0100

    provide start number to flip card
    https://youtu.be/p_6IuhmBsfc?t=1361

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const startNumber = 9;
 const flipCard = document.querySelector(".flip-card");
 
+[...flipCard.children].forEach((child) => {
+  child.textContent = startNumber;
+});
 flipCard.classList.add("flip");
