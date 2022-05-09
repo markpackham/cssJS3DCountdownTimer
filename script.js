1ac0789b48f23d487890d1901d1697ca99c07512
mark <markpackham1@gmail.com>
2022-05-09 15:45:57 +0100

    change variable names for top and bottom
    https://youtu.be/p_6IuhmBsfc?t=1574

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const startNumber = 9;
 const flipCard = document.querySelector(".flip-card");
+const topHalf = flipCard.querySelector(".top");
+const bottomHalf = flipCard.querySelector(".bottom");
 
-[...flipCard.children].forEach((child) => {
-  child.textContent = startNumber;
-});
+topHalf.textContent = startNumber;
+bottomHalf.textContent = startNumber;
 flipCard.dataset.currentNumber = startNumber;
 flipCard.dataset.nextNumber = startNumber - 1;
+flipCard.addEventListener("animationstart", (e) => {
+  topHalf.textContent = startNumber - 1;
+});
+flipCard.addEventListener("animationend", (e) => {
+  bottomHalf.textContent = startNumber - 1;
+});
 flipCard.classList.add("flip");
-flipCard.addEventListener("transitionstart", (e) => {});
-flipCard.addEventListener("transitionend", (e) => {});
