0b587e06cafef2bdeb36246d2ad39004ed6b442d
mark <markpackham1@gmail.com>
2022-05-10 17:02:58 +0100

    correctly round times
    https://youtu.be/p_6IuhmBsfc?t=3201

--- script.js
+++ script.js
@@ -4,9 +4,8 @@ let previouseTimeBetweenDates;
 setInterval(() => {
   currentDate = new Date();
   const timeBetweenDates = Math.ceil((countToDate - currentDate) / 1000);
-  if (previouseTimeBetweenDates !== timeBetweenDates) {
-    flipAllCards(timeBetweenDates);
-  }
+
+  flipAllCards(timeBetweenDates);
 
   previouseTimeBetweenDates = timeBetweenDates;
 }, 250);
@@ -16,10 +15,10 @@ function flipAllCards(time) {
   const minutes = Math.floor(time / 60) % 60;
   const hours = Math.floor(time / 3600);
 
-  flip(document.querySelector("[data-hours-tens]"));
-  flip(document.querySelector("[data-hours-ones]"));
-  flip(document.querySelector("[data-minutes-tens]"));
-  flip(document.querySelector("[data-minutes-ones]"));
+  flip(document.querySelector("[data-hours-tens]"), Math.floor(hours / 10));
+  flip(document.querySelector("[data-hours-ones]"), hours % 10);
+  flip(document.querySelector("[data-minutes-tens]"), Math.floor(minutes / 10));
+  flip(document.querySelector("[data-minutes-ones]"), minutes % 10);
   flip(document.querySelector("[data-seconds-tens]"), Math.floor(seconds / 10));
   flip(document.querySelector("[data-seconds-ones]"), seconds % 10);
 }
